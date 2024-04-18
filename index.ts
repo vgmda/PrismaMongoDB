946926e3d132a6fd154e0d1d68d79282b4c72f6d
vgmda <vasilegancin@gmail.com>
2024-04-18 11:12:47 +0100

    Connect function added

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient();
+
+
+async function main() {
+  // Connect the client
+  await prisma.$connect();
+  const cats = await prisma.cat.findMany();
+  console.log(cats);
+}
+
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
