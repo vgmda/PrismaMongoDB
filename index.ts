4edc32d02c6cb195176bbb144dc2dd09b791bcd7
vgmda <vasilegancin@gmail.com>
2024-04-21 10:56:24 +0100

    Create object

--- index.ts
+++ index.ts
@@ -1,16 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 
-
 const prisma = new PrismaClient();
 
-
 async function main() {
-  // Connect the client
   await prisma.$connect();
-  const cats = await prisma.cat.findMany();
-  console.log(cats);
+  await prisma.cat.create({
+    data: {
+      name: "Sprinkles",
+      color: "Orange",
+      breed: {
+        create: {
+          name: "Tabby",
+        },
+      },
+    },
+  });
+  const cats = await prisma.cat.findMany({
+    include: {
+      breed: true,
+    },
+  });
+  console.dir(cats, { depth: null });
 }
-
 
 main()
   .then(async () => {
